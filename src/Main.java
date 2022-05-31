64a00ada36a31dcf7511605946b10eccdc19d9a2
Anton <imewokrus@gmail.com>
2022-05-31 22:44:59 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        checker("kolbaser", "колбасёр!", "12345678903");
+        checker("kolbaser", "kolbaser", "12345678903");
     }
 
     public static boolean checker(String login, String password, String confirmPassword) {
@@ -9,35 +9,31 @@ public class Main {
             check(login, password, confirmPassword);
         } catch (WrongLoginException e) {
             System.out.println("Login is too long, please try to use shorter login.");
-            counter = counter + 1;
+            return false;
         } catch (WrongLoginException2 e) {
             System.out.println("Login contains non latin letters, please try to use latin letters.");
-            counter = counter + 1;
+            return false;
         } catch (WrongLoginException3 e) {
             System.out.println("Login contains forbidden symbols.");
-            counter = counter + 1;
+            return false;
         } catch (WrongPasswordException e) {
             System.out.println("Passwords do not match!");
-            counter = counter + 1;
+            return false;
         } catch (WrongPasswordException2 e) {
             System.out.println("Passwords contains non latin letters, please try to use latin letters.");
-            counter = counter + 1;
+            return false;
         } catch (WrongPasswordException3 e) {
             System.out.println("Passwords contains forbidden symbols");
-            counter = counter + 1;
+            return false;
         } catch (WrongPasswordException4 e) {
             System.out.println("Passwords is too long, please try to use shorter password.");
-            counter = counter + 1;
-        }
-        if (counter < 1) {
-            return true;
-        } else {
             return false;
         }
-    }
+            return true;
+        }
 
     public static void check(String login, String password, String confirmPassword) throws WrongPasswordException, WrongPasswordException2, WrongPasswordException3, WrongPasswordException4, WrongLoginException, WrongLoginException2, WrongLoginException3 {
-        if (password != confirmPassword) {
+        if (!password.equals(confirmPassword) ) {
             throw new WrongPasswordException();
         }
         if (login.matches("[а-яА-Я]+")) {
@@ -46,7 +42,7 @@ public class Main {
         if (password.matches("[а-яА-Я]+")) {
             throw new WrongPasswordException2();
         }
-        if (password.contains("-* +=)(&?//,.!@#№%^:;$")) {
+        if (password.matches("[Pc[^_]]")) {
             throw new WrongPasswordException3();
         }
         if (password.length() >= 20) {
@@ -55,7 +51,7 @@ public class Main {
         if (login.length() > 20) {
             throw new WrongLoginException();
         }
-        if (login.contains("-* +=)(&?//,.!@#№%^:;$")) {
+        if (login.matches("[Pc[^_]]")) {
             throw new WrongLoginException3();
         }
     }
