d2212d5b6cf861d719f0ef882a89fd82a3254b6a
Anton <imewokrus@gmail.com>
2022-06-01 09:46:48 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -4,55 +4,39 @@ public class Main {
     }
 
     public static boolean checker(String login, String password, String confirmPassword) {
-        int counter = 0;
         try {
             check(login, password, confirmPassword);
         } catch (WrongLoginException e) {
-            System.out.println("Login is too long, please try to use shorter login.");
-            return false;
-        } catch (WrongLoginException2 e) {
-            System.out.println("Login contains non latin letters, please try to use latin letters.");
-            return false;
-        } catch (WrongLoginException3 e) {
-            System.out.println("Login contains forbidden symbols.");
+            System.out.println("Login is too long, or contains forbidden symbols.");
             return false;
         } catch (WrongPasswordException e) {
-            System.out.println("Passwords do not match!");
-            return false;
-        } catch (WrongPasswordException2 e) {
-            System.out.println("Passwords contains non latin letters, please try to use latin letters.");
-            return false;
-        } catch (WrongPasswordException3 e) {
-            System.out.println("Passwords contains forbidden symbols");
-            return false;
-        } catch (WrongPasswordException4 e) {
-            System.out.println("Passwords is too long, please try to use shorter password.");
+            System.out.println("Passwords do not match or contains forbidden symbols.");
             return false;
         }
             return true;
         }
 
-    public static void check(String login, String password, String confirmPassword) throws WrongPasswordException, WrongPasswordException2, WrongPasswordException3, WrongPasswordException4, WrongLoginException, WrongLoginException2, WrongLoginException3 {
+    public static void check(String login, String password, String confirmPassword) throws WrongPasswordException,  WrongLoginException {
         if (!password.equals(confirmPassword) ) {
             throw new WrongPasswordException();
         }
         if (login.matches("[а-яА-Я]+")) {
-            throw new WrongLoginException2();
+            throw new WrongLoginException();
         }
         if (password.matches("[а-яА-Я]+")) {
-            throw new WrongPasswordException2();
+            throw new WrongPasswordException();
         }
         if (password.matches("[Pc[^_]]")) {
-            throw new WrongPasswordException3();
+            throw new WrongPasswordException();
         }
         if (password.length() >= 20) {
-            throw new WrongPasswordException4();
+            throw new WrongPasswordException();
         }
         if (login.length() > 20) {
             throw new WrongLoginException();
         }
         if (login.matches("[Pc[^_]]")) {
-            throw new WrongLoginException3();
+            throw new WrongLoginException();
         }
     }
 }
